30e3e11002d68ef2a38070042e705da3c669a029
seoyeon0516 <0516psy@naver.com>
2026-04-06 12:14:23 +0900

    practice

---
+++ src/sec6/Member.java
@@ -0,0 +1,15 @@
+package sec6;
+
+public class Member {
+    String name;
+    String id;
+    String password;
+    int age;
+
+    Member() {};
+
+    Member(String name, String id) {
+        this.name = name;
+        this.id = id;
+    }
+}
---
+++ src/sec6/MemberExample.java
@@ -0,0 +1,9 @@
+package sec6;
+
+public class MemberExample {
+    public static void main(String[] args) {
+        Member member = new Member();
+        member.name = "최하얀";
+        member.age = 23;
+    }
+}
